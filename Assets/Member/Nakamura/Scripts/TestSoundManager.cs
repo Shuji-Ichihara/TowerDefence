cc334f9db67d21c2746715de09b7367589bccf50
githubyou0805 <noirkun2306@gmail.com>
2024-10-19 17:17:50 +0900

    Add UniTask,SoundManager
    UniTaskとサウンドマネージャの追加
    マネージャで使うBGMとSEも少しだけ追加

---
+++ Assets/Member/Nakamura/Scripts/TestSoundManager.cs
@@ -0,0 +1,31 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using static SoundManager;
+
+
+public class TestSoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private E_BGM _playBGM;
+
+
+    void Update()
+    {
+        // Uを押したらBGMを再生
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            SoundManager.instance.PlayBGM(_playBGM, false);
+        }
+        // Iを押したらフェードアウト
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            SoundManager.instance.FadeIOut(null, 3).Forget();
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SoundManager.instance.PlaySE(SoundManager.E_SE.SE01);
+        }
+
+    }
+}
